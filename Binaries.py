1f2b14b14cc735729c092a7157f82085a797153e
Tullio007 <160785599+Tullio007@users.noreply.github.com>
2024-04-29 19:03:39 -0300

    Add files via upload

---
+++ Binaries.py
@@ -0,0 +1,9 @@
+import math as m
+num = int(input('Write your respective number to see his binare: '))
+numint = m.floor(num)
+bina = ''
+while num > 0:
+    resto = num % 2
+    bina += str(resto)
+    num //= 2
+print(f'The binarie of {numint} is...{bina[::-1]}')
